eab0ef88ddd3af2fb161d16a489b98eb74729fc3
crito <crito@fnordpipe.org>
2018-04-26 02:30:04 +0200

    travis: add travis2discord wrapper

--- src/webhook/travis.lua
+++ src/webhook/travis.lua
@@ -1,7 +1,47 @@
 local http = require('resty.http')
+local rsa = require('resty.rsa')
 local cjson = require('cjson')
+local base64 = require('base64')
 
 local travis = {}
+
+function travis.forward(self, data)
+  local webhook = os.getenv('DISCORD_WEBHOOK_URL')
+  if not webhook then
+    local err = 'missing webhook url'
+    ngx.log(ngx.STDERR, 'travis:' .. err)
+    return ngx.HTTP_INTERNAL_SERVER_ERROR, { reason = err }
+  end
+
+  local content = '```Markdown\n' ..
+    '# ' .. data.type .. ' to ' .. data.branch .. ': ' .. data.status_message .. '\n\n' ..
+    data.message .. '\n' ..
+    '```\n' ..
+    data.build_url
+
+  local req = http:new()
+  local _, code, _, _, body = req:request({
+    url = webhook,
+    scheme = 'https',
+    method = 'POST',
+    headers = {
+      ['Content-Type'] = 'application/json'
+    },
+    body = cjson.encode({
+      content = content,
+      username = 'travis',
+      avatar_url = os.getenv('DISCORD_AVATAR_URL') or nil
+    })
+  })
+
+  if code == 204 then
+    return ngx.HTTP_OK, { reason = 'k thx bye!' }
+  end
+
+  local err = 'discord webhook error (' .. code .. ')'
+  ngx.log(ngx.STDERR, 'travis: ' .. err)
+  return ngx.HTTP_INTERNAL_SERVER_ERROR, { reason = err }
+end
 
 function travis.req_travis_key()
   local req = http:new()
@@ -18,33 +58,52 @@ function travis.req_travis_key()
     local data = cjson.decode(body)
     return data.config.notifications.webhook.public_key
   end
-  return nil
+
+  return nil, 'could not fetch travis\' public key'
 end
 
 function travis.recv_webhook(self)
   ngx.req.read_body()
   local body = ngx.var.request_body
+
   local headers = ngx.req.get_headers()
-  local key = travis:req_travis_key()
+  if not headers['Signature'] then
+    local err = 'missing signature'
+    ngx.log(ngx.STDERR, 'travis: ' .. err)
+    return ngx.HTTP_FORBIDDEN, { reason = err }
+  end
 
+  local key, err = travis:req_travis_key()
   if not key then
-    return ngx.HTTP_INTERNAL_SERVER_ERROR, {
-      reason = 'could not get travis\' public key'
-    }
+    ngx.log(ngx.STDERR, 'travis:' .. err)
+    return ngx.HTTP_INTERNAL_SERVER_ERROR, { reason = err }
   end
 
-  if not travis:verify_req(headers['Signature'], body) then
-    return ngx.HTTP_FORBIDDEN, {
-      reason = 'request signature is wrong or missing'
-    }
+  local verify, err = travis:verify_req(body, headers['Signature'], key)
+  if not verify then
+    ngx.log(ngx.STDERR, 'travis:' .. err)
+    return ngx.HTTP_FORBIDDEN, { reason = err }
   end
 
-  return ngx.HTTP_OK, { msg = 'k thx bye!' }
+  local data = cjson.decode(body)
+  if not data then
+    local err = 'no json body'
+    ngx.log(ngx.STDERR, 'travis: ' .. err)
+    return ngx.HTTP_FORBIDDEN, { reason = err }
+  end
+
+  return travis:forward(data)
 end
 
-function travis.verify_req(self, signature, body)
+function travis.verify_req(self, body, b64signature, key)
   -- see https://docs.travis-ci.com/user/notifications/#Verifying-Webhook-requests
-  return nil
+  local signature = base64.decode(b64signature)
+  local pubkey = rsa:new({ public_key = key, algorithm = 'SHA1' })
+  if not pubkey then return nil, 'cannot read pubkey' end
+  local verify = pubkey:verify(body, signature)
+  if not verify then return nil, 'cannot verify signature' end
+
+  return true
 end
 
 travis.routes = {
